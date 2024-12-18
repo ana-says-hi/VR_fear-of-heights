082cac3f276f11ec144c3e347fc1c49ee21ba32a
ana-says-hi <lujerdeanana@yahoo.com>
2024-12-18 19:04:07 +0200

    elevators almost work

--- Assets/Scripts/elevator_go.cs
+++ Assets/Scripts/elevator_go.cs
@@ -11,25 +11,55 @@ public class elevator_go : MonoBehaviour
     public float forceStrength = 20;
     
     float forceControl;
+    Vector3[] locations;
+
+    private Vector3 targetPosition; // Current target position
+    public float moveSpeed = 5f; // Movement speed
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Vector3 startPos=rb.position;
+
+        //etaje la care se opreste
+        int count = 10;
+        locations = new Vector3[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            float yValue = 0.5f + (i * 5f);
+            locations[i] = startPos + new Vector3(0, yValue, 0);
+        }
+
+        ChooseRandomTarget();
     }
 
     //de mai putine ori decat Update
     private void FixedUpdate()
     {
-        rb.AddForce(Vector3.up * forceControl * forceStrength);
-        //rb.AddForce(Vector3.up * forceStrength);
-        //Vector3 target = target.position - myPosition;
-        //rb.AddForce(target * 15 * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        //rb.AddForce(Vector3.up * forceControl * forceStrength);
+
+        Vector3 direction = (targetPosition - rb.position).normalized;
+        rb.AddForce(direction * forceStrength);
+
+        if (Vector3.Distance(rb.position, targetPosition) < 0.1f)
+        {
+            rb.velocity = Vector3.zero;
+            ChooseRandomTarget();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        forceControl =Input.GetAxis("Vertical");
+       forceControl =Input.GetAxis("Vertical");
+    }
+
+
+    void ChooseRandomTarget()
+    {
+        int randomIndex = Random.Range(0, locations.Length);
+        targetPosition = locations[randomIndex];
     }
 }
