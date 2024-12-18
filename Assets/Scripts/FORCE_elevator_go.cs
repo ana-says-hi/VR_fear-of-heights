7935bd053a1d391c2b6f35b9de1c740d69c8e08d
ana-says-hi <lujerdeanana@yahoo.com>
2024-12-19 00:55:33 +0200

    elevator left seems to go rigth, script upadted

--- Assets/Scripts/FORCE_elevator_go.cs
+++ Assets/Scripts/FORCE_elevator_go.cs
@@ -14,7 +14,7 @@ public class elevator_go : MonoBehaviour
     Vector3[] locations;
 
     private Vector3 targetPosition; // Current target position
-    public float moveSpeed = 2f; // Movement speed
+    public float moveSpeed = 1f; // Movement speed
 
     // Start is called before the first frame update
     void Start()
@@ -23,13 +23,14 @@ public class elevator_go : MonoBehaviour
         Vector3 startPos=rb.position;
 
         //etaje la care se opreste
-        int count = 10;
+        int count = 5;
         locations = new Vector3[count];
 
         for (int i = 0; i < count; i++)
         {
             float yValue = 0.5f + (i * 5f);
             locations[i] = startPos + new Vector3(0, yValue, 0);
+            //locations[i] = new Vector3(0, yValue, 0);
         }
         ChooseRandomTarget();
     }
@@ -42,14 +43,23 @@ public class elevator_go : MonoBehaviour
         Vector3 direction = (targetPosition - rb.position).normalized;
         rb.AddForce(direction * forceStrength);
 
-        //yield new WaitForSeconds(3f);
-        //StartCoroutine(WaitAndChooseNextTarget());
+        if (rb.position.y < 0)
+        {
+            rb.position = new Vector3(rb.position.x, 0, rb.position.z);
+            rb.velocity = Vector3.zero;
+        }
 
-   // Check if close to the target position
-      //  if (Vector3.Distance(rb.position, targetPosition) < 0.1f)
-      //  {
-     //       rb.velocity = Vector3.zero; // Stop movement      
-      //  }
+         // Check if close to the target position
+        if (Vector3.Distance(rb.position, targetPosition) < 0.1f)
+        {
+            rb.velocity = Vector3.zero; // Stop the object
+            StartCoroutine(WaitAndChooseNextTarget());
+            //new WaitForSeconds(3f); // Wait for the specified time
+            //shouldMove = true;
+            //WaitAndChooseNextTarget();
+            //ChooseRandomTarget();
+        }
+
     }
 
      private IEnumerator WaitAndChooseNextTarget()
@@ -69,5 +79,8 @@ public class elevator_go : MonoBehaviour
     {
         int randomIndex = Random.Range(0, locations.Length);
         targetPosition = locations[randomIndex];
+        if(targetPosition.y<0 || targetPosition==rb.position)
+            WaitAndChooseNextTarget();
+           // ChooseRandomTarget();
     }
 }
